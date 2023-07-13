2ee1326e470844b84f5362bbc6fd7feb1fc32aca
ubardev <bellin83@gmail.com>
2023-07-14 02:28:53 +0900

    :art: 5. 등수구하기 풀이

--- Inflearn/Javascript/02_1,2차원배열탐색/5_등수구하기.js
+++ Inflearn/Javascript/02_1,2차원배열탐색/5_등수구하기.js
@@ -1,6 +1,15 @@
 function solution(n, arr) {
-  let answer = 0;
-  // Test
-  return answer;
+  let result = Array.from({ length: n }, () => 1);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i] < arr[j]) {
+        result[i]++;
+      }
+    }
+  }
+
+  return result;
 }
+
 console.log(solution(5, [87, 89, 92, 100, 76]));
