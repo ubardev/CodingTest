dc1b4b4baf1d74b793d34d34860d8a5acbafae9f
ubardev <bellin83@gmail.com>
2023-07-16 06:39:29 +0900

    :sparkles: 1. 회문문자열 reverse사용해서 풀기

--- Inflearn/Javascript/03_문자열탐색/1_회문문자열.js
+++ Inflearn/Javascript/03_문자열탐색/1_회문문자열.js
@@ -1,3 +1,4 @@
+// for문 돌려서 풀기
 function solution(s) {
   let answer = "YES";
   s = s.toLowerCase();
@@ -10,5 +11,12 @@ function solution(s) {
   return answer;
 }
 
+// reverse사용해서 풀기
+function solution2(s) {
+  const str = s.toLowerCase();
+  return str === str.split("").reverse().join("") ? "YES" : "NO";
+}
+
 let str = "gooG";
-console.log(solution(str));
+// console.log(solution(str));
+console.log(solution2(str));
