01927bb58d9e65684d1591a4fd63eb4007510227
ubardev <bellin83@gmail.com>
2023-09-11 05:43:06 +0900

    Feat: 사다리타기 문제 기본 셋팅

---
+++ Inflearn/Javascript.v2/01_시뮬레이션&구현/01_사다리타기.js
@@ -0,0 +1,15 @@
+function simulateLadderGame(n, horizontalLines) {
+  return 0;
+}
+
+const n = 5;
+const horizontalLines = [
+  [1, 3],
+  [3, 5],
+];
+const classPresident = simulateLadderGame(n, horizontalLines);
+console.log(
+  `이번 주 학급회장은 ${String.fromCharCode(
+    64 + classPresident,
+  )}번 학생입니다.`,
+);
